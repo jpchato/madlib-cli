47d22894feb4fc368acaae97d867af81af8e16ac
Jesse Pena <jpchato@gmail.com>
2020-05-13 17:51:37 -0700

    draft

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,25 @@
+print('''Madlib process and welcome message and command line interactions''')
+
+open_file = open('./madlib_cli/testing.txt', 'r')
+print(open_file.read())
+
+# x = input()
+# {Adjective} = x
+
+# for i in open_file:
+#   if i = '{adjective}'
+#     print('xyz')
+
+def read_madlib_file():
+  content = open_file
+  if '{' in content:
+    return [True, content]
+    return potato
+
+def input_function():
+  print('Enter your words here')
+  x = input()
+  list.insert(x)
+
+
+input_function()
---
+++ tests/test_madlib.py
@@ -0,0 +1,11 @@
+from madlib_cli.madlib import open_file
+
+def test_read_open_file():
+  actual = open_file
+#   expected = '''Make Me A Video Game!
+
+# I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb}{A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!
+
+# What are a {Large Animal} and backpacking {Small Animal} to do? Before you can help {A Girl's Name}, you'll have to collect the {Adjective} {Plural Noun} and {Adjective} {Plural Noun} that open up the {Number 1-50} worlds connected to A {First Name's} Lair. There are {Number} {Plural Noun} and {Number} {Plural Noun} in the game, along with hundreds of other goodies for you to find.'''
+  assert actual 
+
